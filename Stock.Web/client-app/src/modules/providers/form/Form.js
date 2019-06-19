b764c5519db4e411c12de2a14dc8c0fddc50235b
therrero <therrero@hexacta.com>
2019-06-19 15:14:43 -0300

    chore: actualizada la carpeta providers/form según linter. #016-FE

--- Stock.Web/client-app/src/modules/providers/form/Form.js
+++ Stock.Web/client-app/src/modules/providers/form/Form.js
@@ -1,42 +1,56 @@
-import React from 'react'
-import { Field } from 'redux-form';
-import { Row, FormGroup } from 'reactstrap'
+import React from "react";
+import { Field } from "redux-form";
+import { Row, Col, FormGroup } from "reactstrap";
 
-import FormToolbar from './Toolbar';
+import FormToolbar from "./Toolbar";
 
-import RenderField from '../../../components/inputs/RenderFieldUpdate'
+import RenderField from "../../../components/inputs/RenderFieldUpdate";
 
+import PropTypes from "prop-types";
 
-export default props => {
-    const {
-        handleSubmit,
-        title,
-    } = props
-    return (
-        <div>
-            <h5> {title} </h5>
-            <div>
-                <form onSubmit={handleSubmit}>
-                    <Row>
-                        <FormGroup className="col-6">
-                            <Field label="Nombre" name="name"
-                                placeholder="Nombre"
-                                component={RenderField}
-                                type="text" />
-                            <Field label="email"
-                                name="email"
-                                placeholder="Marca"
-                                component={RenderField}
-                                type="text" />
-                            <Field label="telefono" name="phone"
-                                placeholder="xxx-xxxx"
-                                component={RenderField}
-                                type="text" />
-                        </FormGroup>
-                    </Row>
-                    <FormToolbar {...props} />
-                </form>
-            </div>
-        </div>
-    )
-}
+const Form = props => {
+  const { handleSubmit, title } = props;
+  return (
+    <div>
+      <h5> {title} </h5>
+      <form onSubmit={handleSubmit}>
+        <FormGroup>
+          <Row>
+            <Col lg="6">
+              <Field
+                label="Nombre"
+                name="name"
+                placeholder="Nombre"
+                component={RenderField}
+                type="text"
+              />
+              <Field
+                label="email"
+                name="email"
+                placeholder="Marca"
+                component={RenderField}
+                type="text"
+              />
+              <Field
+                label="telefono"
+                name="phone"
+                placeholder="xxx-xxxx"
+                component={RenderField}
+                type="text"
+              />
+            </Col>
+          </Row>
+        </FormGroup>
+        <FormToolbar {...props} />
+      </form>
+    </div>
+  );
+};
+
+Form.displayName = "Form";
+Form.propTypes = {
+  handleSubmit: PropTypes.func.isRequired,
+  title: PropTypes.string.isRequired
+};
+
+export default Form;
