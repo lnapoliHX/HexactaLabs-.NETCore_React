d570b83dba93cd271799f6ec5fb8684a9c25b389
Alan <ascheffer@hexacta.com>
2019-06-24 15:57:38 -0300

    chore: limpieza de store module. #016-FE

--- Stock.Web/client-app/src/store/configureStore.js
+++ Stock.Web/client-app/src/store/configureStore.js
@@ -1,43 +1,45 @@
-import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
-import thunk from 'redux-thunk';
-import { connectRouter } from 'connected-react-router'
-import { routerMiddleware } from 'connected-react-router'
-import { reducer as formReducer } from 'redux-form'
+import { applyMiddleware, combineReducers, compose, createStore } from "redux";
+import thunk from "redux-thunk";
+import { connectRouter } from "connected-react-router";
+import { routerMiddleware } from "connected-react-router";
+import { reducer as formReducer } from "redux-form";
 
-
-import auth from '../modules/auth'
-import home from '../modules/home'
-import provider from '../modules/providers'
+import auth from "../modules/auth";
+import home from "../modules/home";
+import provider from "../modules/providers";
 //TODO WHEN API EXISTS
 
 export default function configureStore(history, initialState) {
-    const reducers = {
-        form: formReducer,
-        router: connectRouter(history),
-        auth,
-        home,
-        provider,
-    };
+  const reducers = {
+    form: formReducer,
+    router: connectRouter(history),
+    auth,
+    home,
+    provider
+  };
 
-    const middleware = [
-        thunk,
-        routerMiddleware(history)
-    ];
+  const middleware = [thunk, routerMiddleware(history)];
 
-    const enhancers = [];
-    const isDevelopment = process.env.NODE_ENV === 'development';
+  const enhancers = [];
+  // eslint-disable-next-line no-undef
+  const isDevelopment = process.env.NODE_ENV === "development";
 
-    if (isDevelopment && typeof window !== 'undefined' && window.devToolsExtension) {
-        enhancers.push(window.devToolsExtension());
-    }
+  if (
+    isDevelopment &&
+    typeof window !== "undefined" &&
+    window.devToolsExtension
+  ) {
+    enhancers.push(window.devToolsExtension());
+  }
 
-    const rootReducer = combineReducers({
-        ...reducers
-    });
+  const rootReducer = combineReducers(reducers);
 
-    return createStore(
-        rootReducer,
-        initialState,
-        compose(applyMiddleware(...middleware), ...enhancers)
-    );
+  return createStore(
+    rootReducer,
+    initialState,
+    compose(
+      applyMiddleware(...middleware),
+      ...enhancers
+    )
+  );
 }
