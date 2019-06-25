e88098eadd8eee51c1b472cea1ada0857097ae46
Jonathan Loscalzo <jonathan.r.loscalzo@gmail.com>
2019-06-25 10:25:18 -0300

    linter, cambio == por ===

--- Stock.Web/client-app/src/common/helpers/YupConfig.js
+++ Stock.Web/client-app/src/common/helpers/YupConfig.js
@@ -2,6 +2,6 @@ import * as yup from "yup";
 
 yup.addMethod(yup.number, "format", function(formats, parseStrict) {
   return this.transform(function(value, original) {
-    return original == "" ? null : value;
+    return original === "" ? null : value;
   });
 });
