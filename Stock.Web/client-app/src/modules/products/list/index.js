b9af909bcc28a2c4e5765b54a2ced72e06b00290
Alan Scheffer <ascheffer@fandango.com>
2019-07-02 17:19:19 -0300

    feat: agregado el primer reducer de productos con sus respectivas acciones y selectors. 008-FE

--- Stock.Web/client-app/src/modules/products/list/index.js
+++ Stock.Web/client-app/src/modules/products/list/index.js
@@ -1,10 +1,71 @@
-//import { api } from '../../shared'
+import api from "../../../common/api";
+import { apiErrorToast } from "../../../common/api/apiErrorToast";
 
-const initialState = {};
+const initialState = {
+  loading: false,
+  all: []
+};
 
-export default function reducer(state = initialState, action = {}) {
-  switch (action.type) {
-    default:
-      return state;
-  }
+/* Action Types */
+const LOADING = "PRODUCTS/LOADING";
+const LOAD = "PRODUCTS/LOAD";
+
+/* Reducer */
+function handleLoading(state, { loading }) {
+  return {
+    ...state,
+    loading
+  };
 }
+
+function handleLoad(state, { products }) {
+  return {
+    ...state,
+    all: products
+  };
+}
+
+const handlers = {
+  [LOADING]: handleLoading,
+  [LOAD]: handleLoad
+};
+
+export default function reducer(state = initialState, action) {
+  const handler = handlers[action.type];
+  return handler ? handler(state, action) : state;
+}
+
+/* Actions */
+export function loading(status) {
+  return {
+    type: LOADING,
+    loading: status
+  };
+}
+
+export function load(products) {
+  return {
+    type: LOAD,
+    products
+  };
+}
+
+export function getAll(params = {}) {
+  return function(dispatch) {
+    dispatch(loading(true));
+    return api
+      .get("/product", { params })
+      .then(response => {
+        dispatch(load(response.data));
+        dispatch(loading(false));
+      })
+      .catch(error => {
+        dispatch(loading(false));
+        apiErrorToast(error);
+      });
+  };
+}
+
+/* Selectors */
+export const getProducts = state => state.product.all;
+export const getLoadingStatus = state => state.product.loading;
