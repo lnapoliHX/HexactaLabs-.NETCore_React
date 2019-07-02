65e2098cb38b8afbee3256a364934f655f1da7e1
Alan <ascheffer@hexacta.com>
2019-07-02 11:54:18 -0300

    feat: added boilerplate for products module. 008-FE

---
+++ Stock.Web/client-app/src/modules/products/list/index.js
@@ -0,0 +1,11 @@
+import _ from "lodash";
+//import { api } from '../../shared'
+
+const initialState = {};
+
+export default function reducer(state = initialState, action = {}) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
