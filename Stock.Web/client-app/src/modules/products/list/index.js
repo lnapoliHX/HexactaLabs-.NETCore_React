9e5686820916d45c23dd11c0fb0ddf2c33d444a5
Alan Scheffer <ascheffer@fandango.com>
2019-07-04 15:13:44 -0300

    feat: agregado configuracion de proxy para que el front pueda usar la api. 008-FE

--- Stock.Web/client-app/src/modules/products/list/index.js
+++ Stock.Web/client-app/src/modules/products/list/index.js
@@ -3,15 +3,18 @@ import { apiErrorToast } from "../../../common/api/apiErrorToast";
 
 const initialState = {
   loading: false,
-  all: []
+  all: [],
+  types: []
 };
 
 /* Action Types */
 const LOADING = "PRODUCTS_LOADING";
 const SET = "PRODUCTS_SET";
+const SET_TYPES = "PRODUCTS_TYPES_SET";
 export const ActionTypes = {
   LOADING,
-  SET
+  SET,
+  SET_TYPES
 };
 
 /* Reducer handlers */
@@ -29,9 +32,17 @@ function handleSet(state, { products }) {
   };
 }
 
+function handleSetTypes(state, { types }) {
+  return {
+    ...state,
+    types
+  };
+}
+
 const handlers = {
   [LOADING]: handleLoading,
-  [SET]: handleSet
+  [SET]: handleSet,
+  [SET_TYPES]: handleSetTypes
 };
 
 export default function reducer(state = initialState, action) {
@@ -51,6 +62,13 @@ export function setProducts(products) {
   return {
     type: SET,
     products
+  };
+}
+
+export function setProductTypes(types) {
+  return {
+    type: SET_TYPES,
+    types
   };
 }
 
@@ -74,10 +92,45 @@ export function fetchById(id) {
   return fetchAll({ id });
 }
 
+export function fetchAllTypes() {
+  return function(dispatch) {
+    dispatch(setLoading(true));
+    return api
+      .get("/producttype")
+      .then(response => {
+        dispatch(setProductTypes(response.data));
+        dispatch(setLoading(false));
+      })
+      .catch(error => {
+        dispatch(setLoading(false));
+        apiErrorToast(error);
+      });
+  };
+}
+
+export function fetchTypeById(id) {
+  return function(dispatch) {
+    dispatch(setLoading(true));
+    return api
+      .get(`/producttype/${id}`)
+      .then(response => {
+        dispatch(setProductTypes(response.data));
+        dispatch(setLoading(false));
+      })
+      .catch(error => {
+        dispatch(setLoading(false));
+        apiErrorToast(error);
+      });
+  };
+}
+
 /* Selectors */
+export function getLoading(state) {
+  return state.product.list.loading;
+}
 export function getProducts(state) {
   return state.product.list.all;
 }
-export function getLoading(state) {
-  state.product.list.loading;
+export function getProductTypes(state) {
+  return state.product.list.types;
 }
