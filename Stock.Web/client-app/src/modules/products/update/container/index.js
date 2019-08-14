cdb6520028311c6edd514a1bb028e3b0f75162f7
therrero <therrero@hexacta.com>
2019-08-14 17:00:20 -0300

    Se agregan los proveedores cuando se edita un producto

--- Stock.Web/client-app/src/modules/products/update/container/index.js
+++ Stock.Web/client-app/src/modules/products/update/container/index.js
@@ -6,37 +6,42 @@ import { goBack } from "connected-react-router";
 import Form from "../../form/presentation";
 import { update } from "..";
 import { getProductTypes, getProductById } from "../../list";
+import { getProviders, getAll } from "../../../providers/list";
 
-const Update = ({
-  initialValues,
-  update: onSubmit,
-  goBack: onCancel,
-  productTypeOptions
-}) => {
-  return (
-    <Container fluid>
-      <Row>
-        <h2>Edición</h2>
-      </Row>
-      <Row>
-        <Col>
-          <Form
-            initialValues={initialValues}
-            productTypeOptions={productTypeOptions}
-            onSubmit={onSubmit}
-            handleCancel={onCancel}
-          />
-        </Col>
-      </Row>
-    </Container>
-  );
-};
+class Update extends React.Component {
+  componentDidMount() {
+    this.props.getAll();
+  }
+
+  render() {
+    return (
+      <Container fluid>
+        <Row>
+          <h2>Edición</h2>
+        </Row>
+        <Row>
+          <Col>
+            <Form
+              initialValues={this.props.initialValues}
+              productTypeOptions={this.props.productTypeOptions}
+              providerOptions={this.props.providerOptions}
+              onSubmit={this.props.update}
+              handleCancel={this.props.goBack}
+            />
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
 
 Update.propTypes = {
   productTypeOptions: PropTypes.array.isRequired,
+  providerOptions: PropTypes.array.isRequired,
   initialValues: PropTypes.object.isRequired,
   update: PropTypes.func.isRequired,
-  goBack: PropTypes.func.isRequired
+  goBack: PropTypes.func.isRequired,
+  getAll: PropTypes.func.isRequired
 };
 
 const mapStateToProps = (state, ownProps) => ({
@@ -44,12 +49,17 @@ const mapStateToProps = (state, ownProps) => ({
     label: pt.initials,
     value: pt.id
   })),
+  providerOptions: getProviders(state).map(provider => ({
+    label: provider.name,
+    value: provider.id
+  })),
   initialValues: getProductById(state, ownProps.match.params.id)
 });
 
 const mapDispatchToProps = {
   update,
-  goBack
+  goBack,
+  getAll
 };
 
 export default connect(
